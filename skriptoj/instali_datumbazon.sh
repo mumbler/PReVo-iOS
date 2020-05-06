fe1c6c6c039e74bbdf6ef313a7e4000ee3e63b64
Robin Hill <rehill012@gmail.com>
2020-05-06 09:51:14 -0400

    instal-skripto nun faras savkopiojn de antauaj dosieroj

--- skriptoj/instali_datumbazon.sh
+++ skriptoj/instali_datumbazon.sh
@@ -1,16 +1,44 @@
 #!/bin/sh
 
 produktajhejo="tmp/produktajhoj"
+savkopiejo="savkopioj"
 
 if [ $# -gt 1 ]
 then
     produktajhejo=$1
 fi
 
+if [ $# -gt 2 ]
+then
+    savkopiejo=$2
+fi
+
+datumbaznomo=PoshReVoDatumbazo.sqlite
+datumbazejo=PoshReVo/Risurcoj/Datumbazo
+tekstarnomo=Generataj.strings
+tekstejo=PoshReVo/Risurcoj/Base.lproj/
+
+if [ -e $datumbazejo/$datumbaznomo ] || [ -e $tekstejo/$tekstarnomo ]
+then
+    echo ""
+    echo "== Konservas savkopiojn"
+    mkdir -p $savkopiejo
+
+    if [ -e $datumbazejo/$datumbaznomo ]
+    then
+	cp $datumbazejo/$datumbaznomo $savkopiejo/$datumbaznomo
+    fi
+
+    if [ -e $tekstejo/$testarnomo ]
+    then
+	cp $tekstejo/$tekstarnomo $savkopiejo/$tekstarnomo
+    fi
+fi
+
 echo ""
 echo "== Instalas datumbazon"
-cp $produktajhejo/PoshReVoDatumbazo.sqlite PoshReVo/Risurcoj/Datumbazo/
+cp $produktajhejo/$datumbaznomo $datumbazejo
 
 echo ""
 echo "== Instalas tekstojn"
-cp $produktajhejo/Generataj.strings PoshReVo/Risurcoj/Base.lproj/
+cp $produktajhejo/$tekstarnomo $tekstejo
