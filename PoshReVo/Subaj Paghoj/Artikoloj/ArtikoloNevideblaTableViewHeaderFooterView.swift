9bf3d18ee244723a0bca9c7f70379ddc7ee1a2e1
Robin Hill <rehill012@gmail.com>
2020-07-26 13:25:35 -0400

    Ghustigi profilojn kaj pliigi versionumeron

---
+++ PoshReVo/Subaj Paghoj/Artikoloj/ArtikoloNevideblaTableViewHeaderFooterView.swift
@@ -0,0 +1,9 @@
+//
+//  ArtikoloNevideblaTableViewHeaderFooterView.swift
+//  PoshReVo
+//
+//  Created by Robin Hill on 7/26/20.
+//  Copyright © 2020 Robin Hill. All rights reserved.
+//
+
+import Foundation
